9d7106c895a9a6231e44735b895b87ad3966a280
Ali Hussain Arafat <alihma188@gmail.com>
2025-11-16 15:32:41 +0300

    View product details test cases

--- src/test/java/tests/products_crud/US012BrowsAllProducts.java
+++ src/test/java/tests/products_crud/US012BrowsAllProducts.java
@@ -16,45 +16,24 @@ import static io.restassured.RestAssured.given;
 public class US012BrowsAllProducts extends BazaarStoresBaseUrl {
 
 
+    RequestSpecification spec;
     @BeforeMethod
     public void setUp(){
-        BazaarStoresBaseUrl bazaarStoresBaseUrl = new BazaarStoresBaseUrl();
-//        RequestSpecification spec = bazaarStoresBaseUrl.setStoreManagerSpec();
+        spec = StoreManagerSpec();
     }
 
     @Test
     public void VerifySuccessfulRetrieveOfAllProducts() {
 
         Response response = given(spec).get("/products");
-        response.prettyPrint();
+//        response.prettyPrint();
         response.then()
                 .statusCode(200)
                 .body("", Matchers.notNullValue());
     }
 
     @Test
-    public void VerifySuccessfulRetrieveOfSpecificProduct() {
-        //Get first product id to make test independent
-        try{
-            ObjectMapperUtils.writeJsonToFiles("products_data/productID", given(spec).get("/products"), "[0].id");
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
-        JsonNode payload = ObjectMapperUtils.getJsonNode("/products_data/productID");
-
-        int id = payload.get("[0].id").asInt();
-        spec.pathParam("id",id);
-
-        Response response = given(spec).get("/products/{id}");
-        response.prettyPrint();
-        response.then()
-                .statusCode(200)
-                .body("id", Matchers.equalTo(id));
-    }
-
-    @Test
-    public void VerifyFailOnInvalidToken() {
+    public void VerifyFailGetProductsByInvalidToken(){
 
         RequestSpecification sp = new RequestSpecBuilder()
                 .setBaseUri("https://bazaarstores.com/api")
@@ -64,36 +43,7 @@ public class US012BrowsAllProducts extends BazaarStoresBaseUrl {
                 .build();
 
         Response response = given(sp).get("/products");
-
-        response.prettyPrint();
-        response.then()
-                .statusCode(401)
-                .body("message", Matchers.is("Unauthenticated."));
-    }
-
-    @Test
-    public void VerifyFailGetProductByInvalidId(){
-
-        Response response = given(spec).get("/products/" + "0000000000000000");
-        response.prettyPrint();
-        response.then()
-                .statusCode(404)
-                .body("error", Matchers.is("Product not found"));
-
-    }
-
-    @Test
-    public void VerifyFailGetProductByIdInvalidToken(){
-
-        RequestSpecification sp = new RequestSpecBuilder()
-                .setBaseUri("https://bazaarstores.com/api")
-                .addHeader("Accept", "application/json")
-                .addHeader("Authorization", "Bearer ")
-                .setContentType(ContentType.JSON)
-                .build();
-
-        Response response = given(sp).get("/products/" + "0000000000000000");
-        response.prettyPrint();
+//        response.prettyPrint();
         response.then()
                 .statusCode(401)
                 .body("message", Matchers.is("Unauthenticated."));
